ec941275caa07113e18da06b82f82afe4e4d160a
Luka Barth Moreira Costa <lukabmc@insper.edu.br>
2025-10-29 15:15:25 -0300

    feat: finaliza formulário com inputs de origem e destino

--- src/app/form-busca-passagens/form-busca-passagens.ts
+++ src/app/form-busca-passagens/form-busca-passagens.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-form-busca-passagens',
-  imports: [MatButtonToggleModule],
+  imports: [MatButtonToggleModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './form-busca-passagens.html',
   styleUrl: './form-busca-passagens.scss',
 })
